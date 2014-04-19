1ea400fc3697e39ab5e6788669ecd21c701b3122
astrotycoon <astrotycoon@yahoo.cn>
2014-04-19 14:25:03 +0800

    add info about function dev in readme.txt

--- hello.c
+++ hello.c
@@ -14,11 +14,18 @@ void feature2_print(const char *str)
 	printf("%s", str);
 }
 
+int dev(void)
+{
+	return (1 + 1);
+}
+
 int main(int argc, const char *argv[])
 {
 	printf("hello world.\n");	
 	feature1_print("Hi, everyone. I am feature1 branch");
 	feature1_print("Hi, everyone. I am feature2 branch");
 	
+	dev();
+
 	return 0;
 }
