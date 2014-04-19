67e288c1c85f3a750d100aed1b26cdd8f8190048
astrotycoon <astrotycoon@yahoo.cn>
2014-04-19 13:24:17 +0800

    add frame in master

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void feature1_print(const char *str)
+{
+
+}
+
+void feature2_print(const char *str)
+{
+
+}
+
 int main(int argc, const char *argv[])
 {
 	printf("hello world.\n");	
