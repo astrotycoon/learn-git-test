e2ee70d187598a56445e5e275ddf09a13242a622
astrotycoon <astrotycoon@yahoo.cn>
2014-04-19 13:38:04 +0800

    finish feature2 frame

--- func.c
+++ func.c
@@ -5,3 +5,8 @@ void func1(void)
 {
 	printf("the output of feature1_func is: \n");
 }
+
+void func2(void)
+{
+	printf("the output of feature2_func is: \n");
+}
--- hello.c
+++ hello.c
@@ -9,12 +9,15 @@ void feature1_print(const char *str)
 
 void feature2_print(const char *str)
 {
-
+	func2();
+	printf("%s", str);
 }
 
 int main(int argc, const char *argv[])
 {
 	printf("hello world.\n");	
 	feature1_print("Hi, everyone. I am feature1 branch");
+	feature1_print("Hi, everyone. I am feature2 branch");
+	
 	return 0;
 }
